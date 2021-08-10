d9d195ef6a115804105696e9f1ce016563d309a1
xuqi <xuq3@bngrp.com>
2021-08-10 15:58:49 +0800

    feat: 目录配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,38 @@
-// .vuepress/config.js
+// 侧边栏
 module.exports = {
+    title: 'Jenkins Plus',
+    head: [
+        ['link', {rel: 'shortcut icon', type: "image/x-icon", href: "/images/jenkins-plus.png"}]
+    ],
     themeConfig: {
-        sidebar: {
-            '/持续集成/': [
-                '',     /* /foo/ */
-                '概念介绍',  /* /foo/one.html */
-                '快速开始'   /* /foo/two.html */
-            ],
-            // fallback
-            '/': [
-                '',        /* / */
-                'contact', /* /contact.html */
-                'about'    /* /about.html */
-            ]
-        }
+        nav: [
+            {
+                text: "首页",
+                link: "/",
+            },
+            {
+                text: "使用",
+                link: "/use/",
+            },
+            {
+                text: "讨论区",
+                link: "/team/",
+                // items: [{ text: "代码生成器", link: "/" }],
+            },
+        ],
+        sidebar: [
+            {
+                title: '目录',
+                collapsable: false,
+                path: '/SUMMARY.md'
+            }, {
+                title: '持续集成',
+                collapsable: false,
+                children: [
+                    '/CICD/started',
+                    '/CICD/introduction',
+                ]
+            }
+        ]
     }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,8 @@
 # Jenkins Plus Doc
-hello world!
+
 
 ## 介绍
+
+随着DevOps的理念在众多公司的采纳，Jenkins的Pipeline收到广泛的应用，但Jenkins传统的使用方式相对于开发人员有较高的使用难度， Jenkins Plus将传统的脚本配置变成了可视化、可编排的流水线，让Pipeline更大程度上提升工作效率。
+
+
